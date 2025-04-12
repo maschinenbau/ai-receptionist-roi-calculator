73de46c97d6f9f27c29d0644df0d9c97a1d06ce9
Omar Zamora <ozamora_01@yahoo.com>
2025-04-11 20:55:41 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@
     // Import the main App component (which contains the calculator)
     import App from './App.jsx'
     // Import the CSS file which includes Tailwind directives
-    import './index.css';
+    import './index.css'
 
     // Standard React entry point - renders the App component into the HTML
     ReactDOM.createRoot(document.getElementById('root')).render(
